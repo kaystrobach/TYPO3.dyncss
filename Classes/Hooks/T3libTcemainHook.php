b82dbc9bc6e5ed2e99a6e26c62f499598890ab6b
Kay Strobach <info@kay-strobach.de>
2014-06-25 17:08:34 +0200

    [BUGFIX] just react on some clearCache commands

--- Classes/Hooks/T3libTcemainHook.php
+++ Classes/Hooks/T3libTcemainHook.php
@@ -2,9 +2,11 @@
 
 class tx_dyncss_Hooks_T3libTcemainHook {
 	function clearCachePostProc($params, &$pObj) {
-		t3lib_div::rmdir(
-			PATH_site . 'typo3temp/Cache/Data/DynCss',
-			TRUE
-		);
+		if(isset($params['cacheCmd']) && $params['cacheCmd'] = 'pages') {
+			t3lib_div::rmdir(
+				PATH_site . 'typo3temp/Cache/Data/DynCss',
+				TRUE
+			);
+		}
 	}
 }
