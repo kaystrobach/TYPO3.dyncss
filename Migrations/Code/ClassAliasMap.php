de8825397bec24ce2a7a57164020c80f9f5db2ad
Kay Strobach <info@kay-strobach.de>
2015-01-28 08:06:37 +0100

    [TASK] add classmap for compatibility

---
+++ Migrations/Code/ClassAliasMap.php
@@ -0,0 +1,12 @@
+<?php
+return array(
+	'tx_DynCss_Configuration_BeRegistry' => 'KayStrobach\\DynCss\\Configuration\\BeRegistry',
+	'tx_Dyncss_ExtMgm_Statefield' => 'KayStrobach\\DynCss\\ExtMgm\\Statefield',
+	'tx_Dyncss_Hooks_T3libPageRendererRenderPreProcessHook' => 'KayStrobach\\DynCss\\Hooks\\T3libPageRendererRenderPreProcessHook',
+	'tx_dyncss_Hooks_T3libTcemainHook' => 'KayStrobach\\DynCss\\Hooks\\T3libTcemainHook',
+	'tx_Dyncss_TslibFeContentPostProcAllHook' => 'KayStrobach\\DynCss\\Hooks\\TslibFeContentPostProcAllHook',
+	'tx_Dyncss_Parser_AbstractParser' => 'KayStrobach\\DynCss\\Parser\\AbstractParser',
+	'tx_Dyncss_Parser_DummyParser' => 'KayStrobach\\DynCss\\Parser\\DummyParser',
+	'tx_Dyncss_Parser_ParserInterface' => 'KayStrobach\\DynCss\\Parser\\ParserInterface',
+	'tx_Dyncss_Service_DyncssService' => 'KayStrobach\\DynCss\\Service\\DyncssService',
+);
