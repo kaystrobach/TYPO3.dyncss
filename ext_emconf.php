3abc409ac98c00948f8034f3b1809a79011c18fd
Kay Strobach <github@kay-strobach.de>
2015-03-03 10:38:12 +0100

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,7 +34,7 @@ $EM_CONF[$_EXTKEY] = array(
 	'CGLcompliance_note' => '',
 	'constraints' => array(
 		'depends' => array(
-			'typo3'  => '6.2.0 - 7.0.99'
+			'typo3'  => '6.2.10 - 7.0.99'
 		),
 		'conflicts' => array(
 		),
