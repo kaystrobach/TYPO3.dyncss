d9eb6b55e61678d4f223890ee43d189c9b704ea9
Hendrik Sokolowski <hsokolowski@giftgruen.com>
2015-10-23 18:16:00 +0200

    Added compatibility for toolbar-icon to Typo3-versions below 7.1.

--- Classes/Hooks/Backend/Toolbar/ClearCacheActionsHook.php
+++ Classes/Hooks/Backend/Toolbar/ClearCacheActionsHook.php
@@ -14,47 +14,62 @@ use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class ClearCacheActionsHook implements ClearCacheActionsHookInterface
 {
-    /**
+	/**
      * Modifies CacheMenuItems array.
-     *
-     * @param array $cacheActions Array of CacheMenuItems
-     * @param array $optionValues Array of AccessConfigurations-identifiers (typically  used by userTS with options.clearCache.identifier)
-     *
-     * @return void
-     */
+	 *
+	 * @param array $cacheActions Array of CacheMenuItems
+	 * @param array $optionValues Array of AccessConfigurations-identifiers (typically  used by userTS with options.clearCache.identifier)
+	 *
+	 * @return void
+	 */
     public function manipulateCacheActions(&$cacheActions, &$optionValues)
     {
-        if ($this->getBackendUser()->getTSConfigVal('options.clearCache.system')
-            || GeneralUtility::getApplicationContext()->isDevelopment()
+		if ($this->getBackendUser()->getTSConfigVal('options.clearCache.system')
+			|| GeneralUtility::getApplicationContext()->isDevelopment()
             || ((bool) $GLOBALS['TYPO3_CONF_VARS']['SYS']['clearCacheSystem'] === true && $this->getBackendUser()->isAdmin())) {
+
+
+            /**
+             * Validate Typo3 version and use old core-API for versions below 7.1
+             *
+             * @link https://docs.typo3.org/typo3cms/extensions/core/latest/Changelog/7.1/Deprecation-64922-DeprecatedEntryPoints.html
+             */
+            $hrefParams = ['vC' => $this->getBackendUser()->veriCode(), 'cacheCmd' => 'dyncss', 'ajaxCall' => 1];
+            if (TYPO3_version < '7.1') {
+                $href = 'tce_db.php?' . http_build_query($hrefParams);
+            } else {
+                $href = BackendUtility::getModuleUrl('tce_db', $hrefParams);
+            }
+
+
             $cacheActions[] = [
-                'id'          => 'dyncss',
+				'id' => 'dyncss',
                 'title'       => $this->getLanguageService()->sL('LLL:EXT:dyncss/Resources/Private/Language/locallang.xlf:dyncss.toolbar.clearcache.title', true),
                 'description' => $this->getLanguageService()->sL('LLL:EXT:dyncss/Resources/Private/Language/locallang.xlf:dyncss.toolbar.clearcache.description', true),
-                'href'        => BackendUtility::getModuleUrl('tce_db', ['vC' => $this->getBackendUser()->veriCode(), 'cacheCmd' => 'dyncss', 'ajaxCall' => 1]),
+                'href'        => $href . BackendUtility::getUrlToken('tceAction'),
                 'icon'        => IconUtility::getSpriteIcon('extensions-dyncss-lightning-blue'),
             ];
-            $optionValues[] = 'dyncss';
-        }
-    }
+			$optionValues[] = 'dyncss';
+		}
+	}
 
-    /**
+	/**
      * Returns LanguageService.
-     *
-     * @return \TYPO3\CMS\Lang\LanguageService
-     */
+	 *
+	 * @return \TYPO3\CMS\Lang\LanguageService
+	 */
     protected function getLanguageService()
     {
-        return $GLOBALS['LANG'];
-    }
+		return $GLOBALS['LANG'];
+	}
 
-    /**
-     * Returns the current BE user.
-     *
-     * @return \TYPO3\CMS\Core\Authentication\BackendUserAuthentication
-     */
+	/**
+	 * Returns the current BE user.
+	 *
+	 * @return \TYPO3\CMS\Core\Authentication\BackendUserAuthentication
+	 */
     protected function getBackendUser()
     {
-        return $GLOBALS['BE_USER'];
-    }
+		return $GLOBALS['BE_USER'];
+	}
 }
