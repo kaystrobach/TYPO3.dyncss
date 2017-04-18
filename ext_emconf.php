7049a5f82649687dd10fc6b3a3a01d8261a11078
Dmytro Hrynevych <dmh@users.noreply.github.com>
2017-04-18 12:12:34 +0300

    typo3 8.7 compatibility (#49)
    * typo3 8.7 compatibility
    
    * set 8.7.99 (LTS) instead of 8.99.99

--- ext_emconf.php
+++ ext_emconf.php
@@ -34,7 +34,7 @@ $EM_CONF[$_EXTKEY] = [
     'CGLcompliance_note' => '',
     'constraints'        => [
         'depends' => [
-            'typo3'  => '7.6.15-8.6.99',
+            'typo3'  => '7.6.15-8.7.99',
         ],
         'conflicts' => [
         ],
